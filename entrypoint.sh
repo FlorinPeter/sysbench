dff5fdb342c7a57c2a4ac856002ef319cd303266
Florin Peter <github@florin-peter.de>
2017-10-25 16:18:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ echo "BUILD_REFERENCE: $OPENSHIFT_BUILD_REFERENCE"
 set -e
 
 cd /disk/
-sysbench --test=fileio --file-total-size=$FILE_TOTAL_SIZE --file-num=$FILE_NUM prepare
 
 echo "RUN1 sysbench --test=fileio --file-total-size=$FILE_TOTAL_SIZE --file-test-mode=rndwr --max-time=60 --max-requests=0 --file-block-size=4K --file-num=64 --num-threads=X run"
 for each in 1 4 8 16 32 64; do echo "Threads $each"; sysbench --test=fileio --file-total-size=$FILE_TOTAL_SIZE --file-test-mode=rndwr --max-time=60 --max-requests=0 --file-block-size=4K --file-num=64 --num-threads=$each run; sleep 10; done; 
